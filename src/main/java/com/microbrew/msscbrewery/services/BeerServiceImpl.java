a87774a18b1217d26b06a2c8e08cd1ca9c6af9d7
suddat <suddat.shende@moneyonclick.com>
2021-10-30 10:50:33 +0530

    C-1 : related to delete

--- src/main/java/com/microbrew/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/microbrew/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.microbrew.msscbrewery.services;
 
 import com.microbrew.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
   @Override
@@ -24,5 +26,11 @@ public class BeerServiceImpl implements BeerService {
   @Override
   public void updateBeer(UUID beerId, BeerDto beerDto) {
     //todo impl - will add real implementation in future
+    log.info("updating beer values");
+  }
+
+  @Override
+  public void deleteBeer(UUID beerId) {
+    log.info("Deleting Beer...");
   }
 }
